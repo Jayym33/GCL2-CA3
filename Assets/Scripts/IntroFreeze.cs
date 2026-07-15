38d86867df4f456ce3a96abd8f444eff6384ad94
FloppinFish <163817509+FloppinFish@users.noreply.github.com>
2026-07-15 18:25:16 +0800

    Added IntroFreeze
    Created IntroFreeze Script where mario stops moving for 3 seconds at the start of the game

--- Assets/Scripts/IntroFreeze.cs
+++ Assets/Scripts/IntroFreeze.cs
@@ -1,16 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class IntroFreeze : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Reference your movement script here
+    [SerializeField] private MonoBehaviour PlayerController;
+    [SerializeField] private float freezeDuration = 3f;
+
+    private void Start()
     {
-        
+        if (PlayerController != null)
+        {
+            // Disable movement immediately on load
+            PlayerController.enabled = false;
+
+            // Start the 3-second countdown
+            StartCoroutine(WaitAndEnableMovement());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator WaitAndEnableMovement()
     {
-        
+        // Wait for the specified duration
+        yield return new WaitForSeconds(freezeDuration);
+
+        // Re-enable the movement script
+        PlayerController.enabled = true;
     }
 }
