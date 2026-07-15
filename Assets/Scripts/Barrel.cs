e99c2a50ef55bdcc620df94833e4006c834f9601
FloppinFish <163817509+FloppinFish@users.noreply.github.com>
2026-07-15 12:58:35 +0800

    Added Death by barrel
    Added code where the barrel touches the player it reset the scene (reload scene) can be changed to different scenes if we are deciding on the lose scene or health

--- Assets/Scripts/Barrel.cs
+++ Assets/Scripts/Barrel.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Barrel : MonoBehaviour
 {
     GameObject barrel;
-
-
+ 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +14,19 @@ public class Barrel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(barrel);
+            Debug.Log("Barrel collided");
+            SceneManager.LoadScene("MainLevel");
+
+        }
     }
 }
