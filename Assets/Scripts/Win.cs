5fca4009f379f4dc2caac1cd1e3820d480ccae09
FloppinFish <163817509+FloppinFish@users.noreply.github.com>
2026-07-21 00:00:35 +0800

    Update Win.cs

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -18,12 +18,11 @@ public class Win : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene("WinScreen");
         }
-           
     }
 }
